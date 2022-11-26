07b37ab5c60ba2e2d81a9812026078a906504615
michaeldaranto <36653385+michaeldaranto@users.noreply.github.com>
2022-11-26 23:46:33 +0700

    Add files via upload

--- H743/Src/stm32h7xx_hal_msp.c
+++ H743/Src/stm32h7xx_hal_msp.c
@@ -182,7 +182,7 @@ void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* htim_encoder)
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* TIM3 interrupt Init */
-    HAL_NVIC_SetPriority(TIM3_IRQn, 7, 0);
+    HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(TIM3_IRQn);
   /* USER CODE BEGIN TIM3_MspInit 1 */
 
